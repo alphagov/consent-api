21f43f7244beb18dd56877004cccd3800ba9d5d9
Andy Driver <andy@pagezero.net>
2022-11-28 16:06:37 +0000

    Enable continuous deployment
    * Fix bug that meant CI workflow was not triggered
    * Fix test job to setup a test database and run migrations before tests
    * Remove lint job - pre-commit.ci does this
    * Ignore deprecation warnings to keep test output more readable
    * Refactor database connection config to use DATABASE_URL env var - easier for
      configuring Google Cloud secrets
    * Remove flake8-eradicate - doesn't work with latest flake8 (v6)
    * Add a step to deploy workflow to auto-increment the tag version number and create a
      Github release on pushes to the main branch

--- consent_api/config.py
+++ consent_api/config.py
@@ -1,6 +1,5 @@
 """Flask app configuration."""
 import os
-from urllib.parse import quote
 
 from dotenv import load_dotenv
 
@@ -11,24 +10,4 @@ DEBUG = env.get("DEBUG", True)
 
 SECRET_KEY = env.get("SECRET_KEY", os.urandom(24))
 
-
-def build_db_url(
-    type: str = "",
-    name: str = "",
-    username: str = "",
-    password: str = "",
-    host: str = "",
-    port: str | int = "",
-    sock: str | None = None,
-    **kwargs,
-) -> str:
-    """Construct a database url from parts."""
-    creds = ":".join([quote(s) for s in (username, password) if s])
-    addr = (f"{creds}@" if creds else "") + host + (f":{port}" if port else "")
-    sock = f"?unix_sock={sock}" if sock else ""
-    return f"{type}://{addr}/{name}{sock}"
-
-
-SQLALCHEMY_DATABASE_URI = build_db_url(
-    **{k[3:].lower(): v for k, v in env.items() if k.startswith("DB_")},
-)
+SQLALCHEMY_DATABASE_URI = env["DATABASE_URL"]
