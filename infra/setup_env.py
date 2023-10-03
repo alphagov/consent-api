5b2cf0735abe0a22e5df2b07796e15a9f639b348
Andy Driver <andy@pagezero.net>
2023-10-03 17:01:54 +0100

    Tidy up pulumi scripts
    * JSON/dicts can be used instead of long-named classes in a few places
    * Closures instead of callable objects - a bit less verbose
    * Makes it a bit easier to read

--- infra/setup_env.py
+++ infra/setup_env.py
@@ -1,64 +1,64 @@
 """Pulumi inline program to deploy a stack."""
 
 import argparse
+from collections.abc import Callable
 from pathlib import Path
 
 import pulumi
-import pulumi_gcp as gcp
+from pulumi_gcp import compute
+from pulumi_gcp import iam
+from pulumi_gcp import projects
+from pulumi_gcp import serviceaccount
+from pulumi_gcp import sql
+from pulumi_gcp import storage
 from ruamel.yaml import YAML
 
 
-class EnvironmentStack:
+def setup_env(env: str) -> Callable:
     """Wrapper around Pulumi inline program to pass in variables."""
 
-    def __init__(self, env: str) -> None:
-        """Get the environment and optional tag."""
-        self.env = env
-
-    def __call__(self) -> None:
+    def _setup() -> None:
         """Execute the inline Pulumi program."""
-        gcp.sql.DatabaseInstance(
-            f"{self.env}-db-instance",
+        sql.DatabaseInstance(
+            f"{env}-db-instance",
             database_version=pulumi.Config().require("db-version"),
             deletion_protection=False,
-            settings=gcp.sql.DatabaseInstanceSettingsArgs(
-                tier=pulumi.Config().require("db-tier")
-            ),
+            settings={"tier": pulumi.Config().require("db-tier")},
         )
 
-        github_service_account = gcp.serviceaccount.Account(
+        sa = serviceaccount.Account(
             "service-account",
-            account_id=f"{self.env}",
-            display_name=f"{self.env.title()} Service Account",
+            account_id=f"{env}",
+            display_name=f"{env.title()} Service Account",
         )
 
-        github_wi_pool = gcp.iam.WorkloadIdentityPool.get(
+        wi_pool = iam.WorkloadIdentityPool.get(
             "workload-identity-pool",
-            id=f"{self.env}-github-wi-pool",
+            id=f"{env}-github-wi-pool",
         )
-        if not github_wi_pool:
-            github_wi_pool = gcp.iam.WorkloadIdentityPool(
+        if not wi_pool:
+            wi_pool = iam.WorkloadIdentityPool(
                 "workload-identity-pool",
-                display_name=f"{self.env.title()} Github WI pool",
-                workload_identity_pool_id=f"{self.env}-github-wi-pool",
+                display_name=f"{env.title()} Github WI pool",
+                workload_identity_pool_id=f"{env}-github-wi-pool",
             )
 
-        github_wi_provider = gcp.iam.WorkloadIdentityPoolProvider.get(
+        wi_provider = iam.WorkloadIdentityPoolProvider.get(
             "workload-identity-pool-provider",
             id=pulumi.Output.concat(
-                github_wi_pool.name,
-                f"/providers/{self.env}-github-wi-provider",
+                wi_pool.name,
+                f"/providers/{env}-github-wi-provider",
             ),
         )
-        if not github_wi_provider:
-            github_wi_provider = gcp.iam.WorkloadIdentityPoolProvider(
+        if not wi_provider:
+            wi_provider = iam.WorkloadIdentityPoolProvider(
                 "workload-identity-pool-provider",
-                workload_identity_pool_provider_id=f"{self.env}-github-wi-provider",
-                display_name=f"{self.env.title()} Github",
-                workload_identity_pool_id=github_wi_pool.workload_identity_pool_id,
-                oidc=gcp.iam.WorkloadIdentityPoolProviderOidcArgs(
-                    issuer_uri="https://token.actions.githubusercontent.com",
-                ),
+                workload_identity_pool_provider_id=f"{env}-github-wi-provider",
+                display_name=f"{env.title()} Github",
+                workload_identity_pool_id=wi_pool.workload_identity_pool_id,
+                oidc={
+                    "issuer_uri": "https://token.actions.githubusercontent.com",
+                },
                 attribute_mapping={
                     "attribute.actor": "assertion.actor",
                     "attribute.repository": "assertion.repository",
@@ -69,25 +69,25 @@ class EnvironmentStack:
 
         github_oidc_member = pulumi.Output.format(
             "principalSet://iam.googleapis.com/{wi_pool}/attribute.repository/{repo}",
-            wi_pool=github_wi_pool.name,
+            wi_pool=wi_pool.name,
             repo="alphagov/consent-api",
         )
 
-        gcp.serviceaccount.IAMBinding(
+        serviceaccount.IAMBinding(
             "github-oidc-service-account-binding",
             members=[github_oidc_member],
             role="roles/iam.workloadIdentityUser",
-            service_account_id=github_service_account.name,
+            service_account_id=sa.name,
         )
 
-        gcp.serviceaccount.IAMBinding(
+        serviceaccount.IAMBinding(
             "cloudrun-binding",
-            members=[github_service_account.member],
+            members=[sa.member],
             role="roles/iam.serviceAccountUser",
-            service_account_id=gcp.compute.get_default_service_account().name,
+            service_account_id=compute.get_default_service_account().name,
         )
 
-        github_deployment_role = gcp.projects.IAMCustomRole(
+        deployment_role = projects.IAMCustomRole(
             "deployment-role",
             permissions=[
                 "cloudsql.databases.create",
@@ -109,25 +109,25 @@ class EnvironmentStack:
                 "run.services.setIamPolicy",
                 "run.services.update",
             ],
-            role_id=f"{self.env}_deploy",
-            title=f"{self.env.title()} deployment",
+            role_id=f"{env}_deploy",
+            title=f"{env.title()} deployment",
         )
 
-        gcp.projects.IAMMember(
+        projects.IAMMember(
             "project-deployer",
-            member=github_service_account.member,
+            member=sa.member,
             project="sde-consent-api",
-            role=github_deployment_role.name,
+            role=deployment_role.name,
         )
 
-        gcp.projects.IAMMember(
+        projects.IAMMember(
             "project-cloudsql",
-            member=github_service_account.member,
+            member=sa.member,
             project="sde-consent-api",
             role="roles/cloudsql.admin",
         )
 
-        github_access_to_storage_role = gcp.projects.IAMCustomRole(
+        access_to_storage_role = projects.IAMCustomRole(
             "access-to-storage-role",
             permissions=[
                 "storage.buckets.get",
@@ -136,19 +136,21 @@ class EnvironmentStack:
                 "storage.objects.get",
                 "storage.objects.list",
             ],
-            role_id=f"{self.env}_pushToGCR",
-            title=f"{self.env.title()} push to GCR",
+            role_id=f"{env}_pushToGCR",
+            title=f"{env.title()} push to GCR",
         )
 
-        gcp.storage.BucketIAMBinding(
+        storage.BucketIAMBinding(
             "gcr-binding",
-            bucket=gcp.storage.get_bucket("artifacts.sde-consent-api.appspot.com").name,
-            members=[github_service_account.member],
-            role=github_access_to_storage_role.name,
+            bucket=storage.get_bucket("artifacts.sde-consent-api.appspot.com").name,
+            members=[sa.member],
+            role=access_to_storage_role.name,
         )
 
-        pulumi.export("workload_identity_provider", github_wi_provider.name)
-        pulumi.export("service_account", github_service_account.email)
+        pulumi.export("workload_identity_provider", wi_provider.name)
+        pulumi.export("service_account", sa.email)
+
+    return _setup
 
 
 def main():
@@ -176,13 +178,13 @@ def main():
     stack = pulumi.automation.create_or_select_stack(
         stack_name=stack_name,
         project_name="sde-consent-api",
-        program=EnvironmentStack(args.env),
+        program=setup_env(args.env),
         work_dir=Path(__file__).parent.parent,
     )
     print(f"Initialised stack {stack_name}")
 
     print("Installing plugins...")
-    stack.workspace.install_plugin("gcp", "v6.52.0")
+    stack.workspace.install_plugin("gcp", "v6.67.0")
 
     config_file = Path(__file__).parent / "config" / f"Pulumi.{args.env}.yaml"
     config = YAML(typ="safe").load(config_file)
