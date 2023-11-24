12e8ec1a79a453187eceff251ae82d9c60cd5126
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2023-11-24 21:21:14 +0000

    chore: allow bundling to various JS types
    On top of ES5 IIFE optionally minified:
    * ESmodules
    * CommonJS modules

--- client/rollup.config.mjs
+++ client/rollup.config.mjs
@@ -6,18 +6,40 @@ const baseConfig = {
   plugins: [typescript()],
 }
 
-const unminifiedOutput = {
-  format: 'iife',
-  file: 'dist/singleconsent.js',
-}
-
-const minifiedOutput = {
-  ...unminifiedOutput,
-  file: 'dist/singleconsent.min.js',
-  plugins: [terser()],
-}
-
-export default [
-  { ...baseConfig, output: unminifiedOutput },
-  { ...baseConfig, output: minifiedOutput },
+const bundleConfig = [
+  // ES Module format
+  {
+    ...baseConfig,
+    output: {
+      file: 'dist/singleconsent.esm.js',
+      format: 'esm',
+    },
+  },
+  // CommonJS format
+  {
+    ...baseConfig,
+    output: {
+      file: 'dist/singleconsent.cjs.js',
+      format: 'cjs',
+    },
+  },
+  // IIFE ES5 format (for direct usage in browser via a <script> tag)
+  {
+    ...baseConfig,
+    output: {
+      file: 'dist/singleconsent.iife.js',
+      format: 'iife',
+    },
+  },
+  // Minified IIFE ES5 format (for direct usage in browser via a <script> tag)
+  {
+    ...baseConfig,
+    output: {
+      file: 'dist/singleconsent.iife.min.js',
+      format: 'iife',
+      plugins: [terser()],
+    },
+  },
 ]
+
+export default bundleConfig
