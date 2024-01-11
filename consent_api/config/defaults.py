4689bcff6f64b147282d826474af7ae503d9f0ba
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2024-01-11 09:33:27 +0000

    fix: updated default url production to the correct one

--- consent_api/config/defaults.py
+++ consent_api/config/defaults.py
@@ -4,10 +4,10 @@ class DEV:
 
 
 class STAGING:
-    DEFAULT_CONSENT_API_ORIGIN = "https://gds-single-consent-staging.app/"
+    DEFAULT_CONSENT_API_ORIGIN = "https://gds-single-consent-staging.app"
     DEFAULT_OTHER_SERVICE_ORIGIN = ""
 
 
 class PROD:
-    DEFAULT_CONSENT_API_ORIGIN = ""
+    DEFAULT_CONSENT_API_ORIGIN = "https://consent-api-bgzqvpmbyq-nw.a.run.app"
     DEFAULT_OTHER_SERVICE_ORIGIN = ""
